8fcbed69569fc321d69e664afc48e50f066e3ae1
Haotian_Yu <119282042+hppt7736@users.noreply.github.com>
2024-09-08 11:23:32 +1000

    dragdelete
    dragdelete

---
+++ shapez/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,6 @@
+set(__QT_DEPLOY_TARGET_MyShapez_FILE C:/Users/ICBC/Desktop/shapez/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/MyShapez.exe)
+set(__QT_DEPLOY_TARGET_MyShapez_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_MyShapez_RUNTIME_DLLS D:/astranlia-courses/4/3801/qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/astranlia-courses/4/3801/qt/6.7.2/mingw_64/bin/Qt6Multimedia.dll;D:/astranlia-courses/4/3801/qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/astranlia-courses/4/3801/qt/6.7.2/mingw_64/bin/Qt6Network.dll;D:/astranlia-courses/4/3801/qt/6.7.2/mingw_64/bin/Qt6Core.dll)
+set(__QT_DEPLOY_TARGET_MyShapezLib_FILE C:/Users/ICBC/Desktop/shapez/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/libMyShapezLib.dll)
+set(__QT_DEPLOY_TARGET_MyShapezLib_TYPE SHARED_LIBRARY)
+set(__QT_DEPLOY_TARGET_MyShapezLib_RUNTIME_DLLS D:/astranlia-courses/4/3801/qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/astranlia-courses/4/3801/qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/astranlia-courses/4/3801/qt/6.7.2/mingw_64/bin/Qt6Core.dll)
