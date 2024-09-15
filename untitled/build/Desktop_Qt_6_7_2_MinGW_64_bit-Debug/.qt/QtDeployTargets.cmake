030c46865a369a9f753251ff885a040b3a3f674e
shiromae0 <125993163+shiromae0@users.noreply.github.com>
2024-09-15 08:44:21 +0800

    rollback

--- untitled/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,6 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled_FILE C:/Users/51392/Desktop/Assignment/DECO3801/untitled/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/untitled.exe)
-set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled_RUNTIME_DLLS D:/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
-set(__QT_DEPLOY_TARGET_untitledlib_FILE C:/Users/51392/Desktop/Assignment/DECO3801/untitled/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/libuntitledlib.dll)
-set(__QT_DEPLOY_TARGET_untitledlib_TYPE SHARED_LIBRARY)
-set(__QT_DEPLOY_TARGET_untitledlib_RUNTIME_DLLS D:/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
